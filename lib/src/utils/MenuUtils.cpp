2c9030603920ce4b0eb75207149aa25b9bcefe59
Sven Scharmentke <24257042+svnscha@users.noreply.github.com>
2025-05-01 13:25:57 +0200

    Test: FlashActionHelper segfault fix.

--- lib/src/utils/MenuUtils.cpp
+++ lib/src/utils/MenuUtils.cpp
@@ -6,17 +6,23 @@
 #include <QPointer>
 
 namespace oclero::qlementine {
-class FlashActionHelper : QObject {
+class FlashActionHelper : public QObject {
 public:
   FlashActionHelper(QAction* action, QMenu* menu, const std::function<void()>& onAnimationFinished)
-    : QObject(action)
+    // Set parent to nullptr to manage lifecycle independently from the action
+    : QObject(nullptr)
     , _menu(menu)
     , _action(action)
     , _onAnimationFinished(onAnimationFinished) {
     if (_menu && _action) {
+      // Connect to the action's destroyed signal to prevent accessing it after deletion
+      connect(_action, &QObject::destroyed, this, &FlashActionHelper::onActionDestroyed);
       _action->setProperty("qlementine_flashing", true);
       _menu->blockSignals(true);
       _timerId = startTimer(flashActionBlinkDuration);
+    } else {
+      // If we don't have valid action/menu, clean up immediately
+      QTimer::singleShot(0, this, &QObject::deleteLater);
     }
   }
 
@@ -27,25 +33,42 @@ protected:
       const auto* currentActiveAction = _menu->activeAction();
       _menu->setActiveAction(currentActiveAction == nullptr ? _action : nullptr);
     } else {
-      if (_timerId != -1) {
-        killTimer(_timerId);
-      }
-
-      if (_menu) {
-        if (_action) {
-          _menu->setActiveAction(_action);
-          _action->setProperty("qlementine_flashing", false);
-        }
-        _menu->blockSignals(false);
-      }
-      if (_onAnimationFinished) {
-        _onAnimationFinished();
-      }
-      deleteLater();
+      cleanup();
     }
   }
 
+private slots:
+  void onActionDestroyed() {
+    // Action was destroyed, clear the pointer and clean up
+    _action = nullptr;
+    cleanup();
+  }
+
 private:
+  void cleanup() {
+    if (_timerId != -1) {
+      killTimer(_timerId);
+      _timerId = -1;
+    }
+
+    if (_menu) {
+      if (_action) {
+        _menu->setActiveAction(_action);
+        _action->setProperty("qlementine_flashing", false);
+      }
+      _menu->blockSignals(false);
+    }
+
+    if (_onAnimationFinished) {
+      _onAnimationFinished();
+      // Clear the callback to prevent issues if cleanup is called multiple times
+      _onAnimationFinished = nullptr;
+    }
+
+    // Schedule deletion on next event loop iteration
+    deleteLater();
+  }
+
   static constexpr int flashActionBlinkDuration{ 60 }; // ms
   static constexpr int flashActionDuration{ 2 * flashActionBlinkDuration }; // ms
   int _flashActionElapsedTime{ 0 }; // ms
